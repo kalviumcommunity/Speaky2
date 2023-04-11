3bc1127c586dad4098e07c12e02cd66b6866b35b
Vighnesh <vighneshzagade4@gmail.com>
2023-04-11 15:44:54 +0530

    HomeComponent created

---
+++ Front-End/speaky/src/components/HomeComponent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import image from '../Images/background.jpg'
+
+const HomeComponent = () => {
+    
+  return (
+    <>
+       <div className='main-content'>
+            <h1><b>Speak without fear</b></h1>
+            <img src={image} alt="background"/>
+            <p>With Speaky you can improve your pronunciation which will overall help <br/> you develop your public speaking skills</p>
+            <button> Get Started</button>
+        </div>
+    </>
+  )
+}
+
+export default HomeComponent
