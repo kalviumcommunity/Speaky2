bd2509b1ed5a504f8e8012f6df9e7708585d59cb
Vighnesh <vighneshzagade4@gmail.com>
2023-04-11 15:55:40 +0530

    Navbar Component created

--- Front-End/speaky/src/components/HomeComponent.js
+++ Front-End/speaky/src/components/HomeComponent.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import image from '../Images/background.jpg'
+import Navbar from './Navbar'
 
 const HomeComponent = () => {
     
   return (
     <>
-       <div className='main-content'>
-            <h1><b>Speak without fear</b></h1>
-            <img src={image} alt="background"/>
-            <p>With Speaky you can improve your pronunciation which will overall help <br/> you develop your public speaking skills</p>
-            <button> Get Started</button>
-        </div>
+      <Navbar/>
+      <div className='main-content'>
+          <h1><b>Speak without fear</b></h1>
+          <img src={image} alt="background"/>
+          <p>With Speaky you can improve your pronunciation which will overall help <br/> you develop your public speaking skills</p>
+          <button> Get Started</button>
+      </div>
     </>
   )
 }
