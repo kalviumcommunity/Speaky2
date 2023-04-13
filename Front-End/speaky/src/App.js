f737320272082fa542f0b1c6d90a1da1cebf93d1
Vighnesh <vighneshzagade4@gmail.com>
2023-04-13 09:56:25 +0530

    Changes made

--- Front-End/speaky/src/App.js
+++ Front-End/speaky/src/App.js
@@ -21,8 +21,10 @@ function App() {
 
   return (
     <div className="App">
-      <Auth0Provider domain="dev-ygqecl3ixouydlqi.us.auth0.com" clientId="szit4jzw3sbGyyW7X88p6DhU7bcmvIPl"
-       authorizationParams={{
+      <Auth0Provider 
+        domain={process.env.REACT_APP_AUTH0_DOMAIN} 
+        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+        authorizationParams={{
           redirect_uri: window.location.origin
         }}>
         <BrowserRouter>
